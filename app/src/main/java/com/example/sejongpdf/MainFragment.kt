300386d69962aa9c63b1037ea7b8ada679fdded0
sprisao <whdtmek@gmail.com>
2023-06-08 16:33:30 +0900

    추가 [약제비영수증] : 기본 뷰 추가

--- app/src/main/java/com/example/sejongpdf/MainFragment.kt
+++ app/src/main/java/com/example/sejongpdf/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.sejongpdf.models.PharmacyReceipt
+import com.google.gson.Gson
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -12,6 +14,52 @@ private const val ARG_PARAM2 = "param2"
 class MainFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
+//
+//    val jsonString = """
+//        {
+//            "receiptNumber": "1234567890",
+//            "patientName": "홍길동",
+//            "formulationDate": "2021-09-01T00:00:00",
+//            "medicationDays": 30,
+//            "medicines": [
+//                {
+//                    "name": "약품1",
+//                    "price": 1000,
+//                    "dosage": "1회 1정",
+//                    "administrationTimes": 3,
+//                    "administrationDays": 30
+//                },
+//                {
+//                    "name": "약품2",
+//                    "price": 2000,
+//                    "dosage": "1회 1정",
+//                    "administrationTimes": 3,
+//                    "administrationDays": 30
+//                }
+//            ],
+//            "costStructure": [
+//                {
+//                    "name": "약값",
+//                    "amount": 3000
+//                },
+//                {
+//                    "name": "조제료",
+//                    "amount": 1000
+//                },
+//                {
+//                    "name": "기타",
+//                    "amount": 1000
+//                }
+//            ],
+//            "businessNumber": "123-45-67890",
+//            "businessName": "약국명",
+//            "purchaseDate": "2021-09-01T00:00:00",
+//            "totalAmount": 5000
+//        }
+//    """.trimIndent()
+//
+//    private val gson = Gson()
+//    val receipt: PharmacyReceipt = gson.fromJson(jsonString, PharmacyReceipt::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +76,13 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+    }
+
+
+    companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             MainFragment().apply {
---
+++ app/src/main/java/com/example/sejongpdf/models/data.kt
@@ -0,0 +1,30 @@
+package com.example.sejongpdf.models
+
+import com.google.gson.annotations.SerializedName
+import java.time.LocalDateTime
+
+data class PharmacyReceipt(
+    @SerializedName("receiptNumber") val receiptNumber: String,
+    @SerializedName("patientName") val patientName: String,
+    @SerializedName("formulationDate") val formulationDate: LocalDateTime,
+    @SerializedName("medicationDays") val medicationDays: Int,
+    @SerializedName("medicines") val medicines: List<Medicine>,
+    @SerializedName("costStructure") val costStructure: List<CostItem>,
+    @SerializedName("businessNumber") val businessNumber: String,
+    @SerializedName("businessName") val businessName: String,
+    @SerializedName("purchaseDate") val purchaseDate: LocalDateTime,
+    @SerializedName("totalAmount") val totalAmount: Double
+)
+
+data class Medicine(
+    @SerializedName("name") val name: String,
+    @SerializedName("price") val price: Double,
+    @SerializedName("dosage") val dosage: String,
+    @SerializedName("administrationTimes") val administrationTimes: Int,
+    @SerializedName("administrationDays") val administrationDays: Int
+)
+
+data class CostItem(
+    @SerializedName("name") val name: String,
+    @SerializedName("amount") val amount: Double
+)
